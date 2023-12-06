3b8abad3db1cfdcb99ed87e85705bcbf294f3224
madj0ng <madj0ng@yandex.ru>
2023-12-06 13:11:33 +0300

    Sprint_8_Homework: First Commit

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class SettingsActivity : AppCompatActivity() {
 
         // Нажатие иконки экрана Настройки
         val back = findViewById<ImageView>(R.id.back)
-        back.setOnClickListener({
-            startActivity(Intent(this, MainActivity::class.java))
-        })
+        back.setOnClickListener {
+            super.finish()
+        }
     }
 }
